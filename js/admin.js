b5914971ed94ca566cc8506944896bed9756408e
Martin Rodriguez <neomartinr@gmail.com>
2023-09-28 21:58:54 -0300

    Funcion agregar y borrar completas

--- js/admin.js
+++ js/admin.js
@@ -7,24 +7,18 @@
     - categoria
     - id (automatico)
 */
-const formularioProductoHTML = document.getElementById("formularioProducto")
-
-formularioProductoHTML.addEventListener('submit', () => {
-    
-    alert(`Evento submit`)
-
-})
-
 const consolas = [
     {
+        id: '5f49fab9-3135-4676-a160-5c3fdbb1ae92',
         descripcion: 'Consola de hogar con gráficos en alta definición.',
         titulo: 'PlayStation 5',
-        fechaDeCreacion: '2020-11-12',
         precio: 499.99,
         imagen: 'https://stylewatch.vtexassets.com/arquivos/ids/233580-800-800?v=638188815205330000&width=800&height=800&aspect=true',
-        categoria: 'Consola de Hogar'
+        categoria: 'Consola de Hogar',
+        fechaDeCreacion: '2020-11-12',
     },
     {
+        id: '2e897bad-d4e4-413d-a515-ed95df9ad917',
         fechaDeCreacion: '2017-03-03',
         descripcion: 'Consola portátil con pantalla táctil.',
         titulo: 'Nintendo Switch',
@@ -33,6 +27,7 @@ const consolas = [
         categoria: 'Consola Portátil'
     },
     {
+        id: 'fc3025ec-f314-4b63-9765-1e8df3ee358a',
         descripcion: 'Consola de hogar con alta potencia y compatibilidad hacia atrás.',
         titulo: 'Xbox Series X',
         fechaDeCreacion: '2020-11-10',
@@ -41,6 +36,7 @@ const consolas = [
         categoria: 'Consola de Hogar'
     },
     {
+        id: 'eec389d2-3d56-4b74-ae13-5d387a592048',
         descripcion: 'Consola clásica miniaturizada con juegos preinstalados.',
         titulo: 'NES Classic Edition',
         fechaDeCreacion: '2016-11-10',
@@ -50,25 +46,91 @@ const consolas = [
     }
 ];
 const tableBodyHTML = document.querySelector("#table-body")
+// Pintamos productos al cargar nuestro script por primera vez
+pintarProductos()
+const inputFiltrarHTML = document.getElementById("filtrar")
 
-// Crear una función que sea pintar elementos
-// Blanquear el HTML
-consolas.forEach(function(conso) {
-    
-    tableBodyHTML.innerHTML += 
-        `<tr>
-            <td class="table-image">
-                    <img src="${conso.imagen}" alt="${conso.titulo}">
-            </td>
-            <td class="table-title">${conso.titulo}</td>
-            <td class="table-description">${conso.descripcion}</td>
-            <td class="table-price">${conso.precio}</td>
-            <td class="table-category">${conso.categoria}</td>
-        </tr>`
+const formularioProductoHTML = document.getElementById("formularioProducto")
 
+// !LISTENER EVENTO FORMULARIO
+formularioProductoHTML.addEventListener('submit', (evt) => {
+
+    evt.preventDefault()
+
+    const el = formularioProductoHTML.elements;
+
+    const nuevoProducto = {
+        id: crypto.randomUUID(),
+        titulo: el.tituloName.value,
+        descripcion: el.descripcion.value,
+        precio: el.precio.valueAsNumber,
+        imagen: el.imagen.value,
+        categoria: el.categoria.value,
+        fechaDeCreacion: obtenerFecha(),
+    }
+
+    consolas.push(nuevoProducto)
+    pintarProductos()
+    formularioProductoHTML.reset()
+    el.tituloName.focus()
 })
 
 
+
+
+
+function pintarProductos() {
+    tableBodyHTML.innerHTML = "";
+
+    consolas.forEach(function(conso, index) {
+        tableBodyHTML.innerHTML += 
+            `<tr>
+                <td class="table-image">
+                        <img src="${conso.imagen}" alt="${conso.titulo}">
+                </td>
+                <td class="table-title">${conso.titulo}</td>
+                <td class="table-description">${conso.descripcion}</td>
+                <td class="table-price">${conso.precio}</td>
+                <td class="table-category">${conso.categoria}</td>
+                <td>
+                    <button class="btn btn-danger btn-sm" onclick="borrarProducto(${index})">
+                        <i class="fa-solid fa-trash"></i>
+                    </button>
+                
+                </td>
+            </tr>`
+    })
+}
+
+
+//Funcion para filtrar productos
+inputFiltrarHTML.addEventListener('keyup', (evt) => {
+    console.log(evt.target.value )
+})
+
+function borrarProducto(indiceRecibido) {
+    consolas.splice(indiceRecibido, 1)
+    pintarProductos()
+}
+
+
+
+
+function obtenerFecha() {
+    const fecha = new Date()
+    let mes = fecha.getMonth() + 1;
+    if(mes < 10) {
+        mes = '0'+ mes
+    }
+    let dia = fecha.getDate()
+    if(dia < 10) {
+        dia = '0' + dia
+    }
+    const year = fecha.getFullYear()
+
+    const fechaFormateada = `${year}-${mes}-${dia}`
+    return fechaFormateada
+}
 
 
 
@@ -85,3 +147,4 @@ consolas.forEach(function(conso) {
         <td class="table-price">1000</td>
         <td class="table-category">Consolas</td>
     </tr> */
+
