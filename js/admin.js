553a9e67c25149bf4e1bd7d9efc4ab59d393ca2d
Martin Rodriguez <neomartinr@gmail.com>
2023-10-05 20:34:11 -0300

    Editar productos casi termiando

--- js/admin.js
+++ js/admin.js
@@ -7,6 +7,7 @@
     - categoria
     - id (automatico)
 */
+
 let consolas = [
     {
         id: '5f49fab9-3135-4676-a160-5c3fdbb1ae92',
@@ -14,7 +15,7 @@ let consolas = [
         titulo: 'PlayStation 5',
         precio: 499.99,
         imagen: 'https://stylewatch.vtexassets.com/arquivos/ids/233580-800-800?v=638188815205330000&width=800&height=800&aspect=true',
-        categoria: 'Consola de Hogar',
+        categoria: 'consolas',
         fechaDeCreacion: '2020-11-12',
     },
     {
@@ -24,7 +25,7 @@ let consolas = [
         titulo: 'Nintendo Switch',
         precio: 299.99,
         imagen: 'https://http2.mlstatic.com/D_NQ_NP_625423-MLA47920375564_102021-O.webp',
-        categoria: 'Consola Portátil'
+        categoria: 'accesorios'
     },
     {
         id: 'fc3025ec-f314-4b63-9765-1e8df3ee358a',
@@ -33,7 +34,7 @@ let consolas = [
         fechaDeCreacion: '2020-11-10',
         precio: 499.99,
         imagen: 'https://assets.xboxservices.com/assets/fb/d2/fbd2cb56-5c25-414d-9f46-e6a164cdf5be.png?n=XBX_A-BuyBoxBGImage01-D.png',
-        categoria: 'Consola de Hogar'
+        categoria: 'juegos'
     },
     {
         id: 'eec389d2-3d56-4b74-ae13-5d387a592048',
@@ -42,7 +43,7 @@ let consolas = [
         fechaDeCreacion: '2016-11-10',
         precio: 59.99,
         imagen: 'https://m.media-amazon.com/images/I/81s7B+Als-L._SL1500_.jpg',
-        categoria: 'Consola Retro'
+        categoria: 'consolas'
     },
     {
         id: '5732bbf2-fa29-43c5-9acd-8246334ff9ea',
@@ -50,7 +51,7 @@ let consolas = [
         titulo: 'Nintendo DS',
         fechaDeCreacion: '2004-11-21',
         precio: 149.99,
-        categoria: 'Consola Portátil',
+        categoria: 'accesorios',
         imagen: 'https://assets.stickpng.com/images/585ea26acb11b227491c3509.png'
     },
     {
@@ -59,7 +60,7 @@ let consolas = [
         titulo: 'Nintendo Wii U',
         fechaDeCreacion: '2012-11-18',
         precio: 299.99,
-        categoria: 'Consola de Hogar',
+        categoria: 'consolas',
         imagen: 'https://nucleogamer.com/wp-content/uploads/492448_thumb-1.png',
     },
     {
@@ -68,7 +69,7 @@ let consolas = [
         titulo: 'PlayStation 4',
         fechaDeCreacion: '2013-11-15',
         precio: 399.99,
-        categoria: 'Consola de Hogar',
+        categoria: 'juegos',
         imagen: 'https://livingplaystation.com/wp-content/uploads/2018/11/playstation-4-pro.png',
     },
     {
@@ -78,9 +79,10 @@ let consolas = [
         titulo: 'Xbox 360',
         fechaDeCreacion: '2005-11-22',
         precio: 299.99,
-        categoria: 'Consola de Hogar'
+        categoria: 'consolas'
     }
 ];
+let idEditar;
 const tableBodyHTML = document.querySelector("#table-body")
 // Pintamos productos al cargar nuestro script por primera vez
 pintarProductos(consolas)
@@ -95,8 +97,18 @@ formularioProductoHTML.addEventListener('submit', (evt) => {
 
     const el = formularioProductoHTML.elements;
 
+    // let id = idEditar === undefined ? crypto.randomUUID() : idEditar
+    let id;
+
+    if(idEditar) {
+        id = idEditar
+    } else {
+        id = crypto.randomUUID()
+    }
+
+
     const nuevoProducto = {
-        id: crypto.randomUUID(),
+        id: id,
         titulo: el.tituloName.value,
         descripcion: el.descripcion.value,
         precio: el.precio.valueAsNumber,
@@ -105,8 +117,22 @@ formularioProductoHTML.addEventListener('submit', (evt) => {
         fechaDeCreacion: obtenerFecha(),
     }
 
-    consolas.push(nuevoProducto)
-    pintarProductos()
+    if(idEditar) {
+        const index = consolas.findIndex(consola => {
+            return consola.id === idEditar
+        })
+
+        consolas[index] = nuevoProducto;
+
+
+    } else {
+        consolas.push(nuevoProducto)
+
+    }
+
+
+    pintarProductos(consolas)
+    
     formularioProductoHTML.reset()
     el.tituloName.focus()
 })
@@ -116,6 +142,7 @@ formularioProductoHTML.addEventListener('submit', (evt) => {
 
 
 function pintarProductos(arrayAPintar) {
+
     tableBodyHTML.innerHTML = "";
 
     arrayAPintar.forEach(function(conso, index) {
@@ -190,15 +217,95 @@ const borrarProducto = (idABuscar) => {
     // obtenerBotones()
 }
 
-
+// - Editar producto
 const editarProducto = function(idRecibido) {
     console.log(`Editar elemento ${idRecibido}`)
+    // Cuando la personas clickea en el botón editar me manda un ID
+    // Buscar el producto que posee dicho id
+            //#Opcion 1
+            // const productoEditar = consolas.filter(prod => {
+            //     if(idRecibido === prod.id) {
+            //         return true
+            //     }
+            //     return false
+            // })
+
+            // console.log(productoEditar[0])
+
+            //#Opcion 2
+            // const prodIndex = consolas.findIndex(prod => {
+                
+            //     if(prod.id === idRecibido) {
+            //         return true
+            //     }
+            
+            // })
+
+            // if(prodIndex < 0) return;
+
+            // const productoEditar = consolas[0]
+
+            //#Opcion 3 - RECOMENDADA
+    const productoEditar = consolas.find((prod) => {
+        if(prod.id === idRecibido) {
+            return true
+        }
+    })
+
+    // Resguardo si find no encontro nada = undefined
+    if(!productoEditar) return;
+
+    idEditar = productoEditar.id
+
+    // Deberíamos rellenar nuestro formulario con los datos del elemento a editar
+    const elements = formularioProductoHTML.elements;
+
+    elements.tituloName.value = productoEditar.titulo;
+    elements.precio.value = productoEditar.precio;
+    elements.descripcion.value = productoEditar.descripcion;
+    elements.imagen.value = productoEditar.imagen;
+    elements.categoria.value = productoEditar.categoria;
+    // Modificar el botón de agregar con uno de editar
+    // Luego reemplazar el producto anterior con los datos nuevos
+
+    const btn = document.querySelector('button.btn[type="submit"]')
+    btn.innerText = "Editar Producto"
+    btn.classList.add("btn-success")
+
+
+
+    // btn.innerText = "Agregar producto"
+    // btn.classList.remove("btn-success")
 }
 
 
 
 
+/*
+    Duplicar array y evitar la misma referencia entre ambos
+    array1 = [1,2,3,4]
+    #1 Opcion
+    array2 = array1.slice()
 
+    #2 Opcion
+    array2 = Array.from(array1)
+
+    #3 Opcion
+    array2 = array1.map(elem => {
+        return elem
+    })
+    array2 = array1.map(elem => elem)
+
+    #4 Opcion
+    array2 = [ ...array1 ]
+
+    #5 Opcion
+    - array2 = structuredClone(array1)
+
+    #1 Opcion objetos
+    const obj2 = { ...obj1 }
+
+*/
 
 
 
